c98e3df5e3b6926462dbb756e0cb920f75509b70
Matheus Nery <matheusnery345@gmail.com>
2022-11-01 09:46:44 -0400

    Criação do CATEGORIA-READ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,31 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { SidenavComponent } from './components/template/sidenav/sidenav.component';
+import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 import { HomeComponent } from './components/views/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 /* ANGULAR MATERIAL */
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatCardModule } from '@angular/material/card';
-
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     SidenavComponent,
     HomeComponent,
+    CategoriaReadComponent,
   ],
   imports: [
     BrowserModule,
@@ -35,6 +38,8 @@ import { MatCardModule } from '@angular/material/card';
     MatListModule,
     MatTooltipModule,
     MatCardModule,
+    MatTableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/views/categoria/categoria-read/categoria-read.component.ts
@@ -0,0 +1,25 @@
+import { CategoriaService } from './../categoria.service';
+import { Categoria } from './../categoria.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categoria-read',
+  templateUrl: './categoria-read.component.html',
+  styleUrls: ['./categoria-read.component.css'],
+})
+export class CategoriaReadComponent implements OnInit {
+  categorias: Categoria[] = [];
+  displayedColumns: string[] = ['id', 'nome', 'desc', 'action'];
+
+  constructor(private service: CategoriaService) {}
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll(): void {
+    this.service.findAll().subscribe((resposta) => {
+      this.categorias = resposta;
+    });
+  }
+}
